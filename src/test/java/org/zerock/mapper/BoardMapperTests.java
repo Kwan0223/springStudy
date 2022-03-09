57d28a372db65ac1480dccdb88c668898f5d8a6d
[kwanyol] <pkl824000@gmail.com>
2022-03-09 21:29:20 +0900

    first commit

---
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,72 @@
+package org.zerock.mapper;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+	@Autowired
+	private BoardMapper boardMapper;
+	
+	@Test
+	public void testGetList() {
+		log.info("----------------");
+		boardMapper.getList();
+		
+	}
+	
+	@Test
+	public void testInsert() {  // 그냥 글이 생성되었다 할떄 이렇게!!
+		 BoardVO vo = new BoardVO();
+		 vo.setTitle("Test 테스트");
+		 vo.setContent("Content 테스트");
+		 vo.setWriter("tester");
+		 
+		 boardMapper.insert(vo);
+		 log.info("-------------------");
+		 log.info("after insert " + vo.getBno());
+	}
+	@Test
+	public void testInsertSelectKey(){  // 몇번 글이 생성되었습니다 안내할떈 이렇게 !!
+		 BoardVO vo = new BoardVO();
+		 vo.setTitle("AAATest 테스트");
+		 vo.setContent("AAAContent 테스트");
+		 vo.setWriter("AAAtester");
+		 
+		 boardMapper.insertSelectKey(vo); 
+		 log.info("-------------------");
+		 log.info("after insert selectkey " + vo.getBno());
+	}
+	@Test
+	public void testRead() {
+		BoardVO vo = boardMapper.read(4L); //25번쨰 long 타입 
+		log.info(vo);
+	}
+	@Test 
+	public void testDelete(){
+		int count = boardMapper.delete(4L);
+		
+		log.info("count : " + count);
+	}
+
+	@Test
+	public void testUpdate() {
+		BoardVO vo = new BoardVO();
+		
+		vo.setBno(1L);
+		vo.setTitle("Update Title");
+		vo.setContent("Update Content");
+		vo.setWriter("user00");
+		
+		log.info("count :" + boardMapper.update(vo));
+	}
+}
