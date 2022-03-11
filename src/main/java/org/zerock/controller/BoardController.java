ebeeecfbc39617e4e2adeef95d0580bf6ddb4e9b
[kwanyol] <pkl824000@gmail.com>
2022-03-11 19:52:30 +0900

    부트스트랩 템플릿 적용 / 게시물 화면 및 처리 / 한글필터 처리 / 목록테스트 모델창

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -29,6 +29,10 @@ public class BoardController {
 		model.addAttribute("list", service.getList());
 		
 	}
+	@GetMapping("/register")
+	public void registerGET() {
+		
+	}
 	
 	@PostMapping("/register")
 	public String register(BoardVO board, RedirectAttributes rttr) {
